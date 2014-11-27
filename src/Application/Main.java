4c9572dcb2b8abccd496669833e78fb6480d532d
usuario <usuario@10.22.143.12>
2014-11-27 12:01:02 +0000

    añadida interfaz

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -5,11 +5,13 @@ import Persistance.CurrencySetLoader;
 
 
 
+
 public class Main {
 
     public static void main(String[] args) {
         CurrencySetLoader currencySetLoader = new CurrencySetLoader();
         currencySetLoader.load();
+        
         new ExchangeOperation().execute();
     }
     
